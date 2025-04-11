f26902bdb975d182f49cc202194a57bba38e7bbd
Yuliia Nedavnia <nedavaia@gmail.com>
2025-04-11 16:30:01 +0300

    ThreadStatic attribute - makes shared valiable local for each thread

--- MultithreadingConsoleApp/Program.cs
+++ MultithreadingConsoleApp/Program.cs
@@ -1,6 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
 
-using MultithreadingConsoleApp;
+class Program
+{
+    [ThreadStatic]
+    public static int _counter;
+    static void Main()
+    {
+        Thread t1 = new Thread(IncrementThreadLocal);
+        Thread t2 = new Thread(IncrementThreadLocal);
+        
+        t1.Start();
+        t2.Start();
+        
+        t1.Join();
+        t2.Join();
+    }
 
-var fibonacciCounter = new FibonacciCounter();
-fibonacciCounter.PrintFibonacciSequence(200);
+    static void IncrementThreadLocal()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            _counter++;
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: {_counter}");
+        }
+    }
+}
