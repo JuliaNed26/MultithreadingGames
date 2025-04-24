63082e53d5982bf30e2ef0e9501ec54660e22f3f
Yuliia Nedavnia <nedavnaia@gmail.com>
2025-04-24 18:49:44 +0300

    Lock

---
+++ MultithreadingConsoleApp/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿namespace MultithreadingConsoleApp;
+
+public class BankAccount
+{
+    private static readonly object _locker = new();
+
+    public long Balance { get; private set; }
+
+    public void Deposit(long sum)
+    {
+        lock (_locker)
+        {
+            Balance += sum;
+        }
+    }
+
+    public void Withdraw(long sum)
+    {
+        lock (_locker)
+        {
+            Balance -= sum;
+        }
+    }
+}
--- MultithreadingConsoleApp/Program.cs
+++ MultithreadingConsoleApp/Program.cs
@@ -1,19 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using MultithreadingConsoleApp;
-var countdownService = new CountdownService();
-Console.WriteLine("Write number to start countdoun from: ");
-var countdownNumber = Console.ReadLine();
 
-var cancellationTokenSource = new CancellationTokenSource();
-var token = cancellationTokenSource.Token;
-var countdownThread1 = new Thread(() => countdownService.IncreaseLocalVariable(token));
-var countdownThread2 = new Thread(() => countdownService.IncreaseLocalVariable(token));
+var bankAccount = new BankAccount();
 
-countdownThread1.Name = "Thread 1";
-countdownThread2.Name = "Thread 2";
-countdownThread1.Priority = ThreadPriority.Highest;
-countdownThread2.Priority = ThreadPriority.Normal;
-countdownThread1.Start();
-countdownThread2.Start();
-cancellationTokenSource.CancelAfter(TimeSpan.FromSeconds(30));
+int iterations = 100;
+var depositThread = new Thread(() =>
+{
+    for (int i = 0; i < iterations; i++)
+    {
+        bankAccount.Deposit(100);
+    }
+});
+var withdrawalThread = new Thread(() =>
+{
+    for (int i = 0; i < iterations; i++)
+    {
+        bankAccount.Withdraw(50);
+    }
+});
+depositThread.Start();
+withdrawalThread.Start();
+depositThread.Join();
+withdrawalThread.Join();
+
+Console.WriteLine($"Balance: {bankAccount.Balance}");
